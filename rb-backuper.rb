2ee64ac414bad7982475ccfa064c34a6fefc7419
cvb <peter@standalone.su>
2011-01-29 18:01:48 +0300

    Add args parser.

--- rb-backuper.rb
+++ rb-backuper.rb
@@ -14,23 +14,49 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 require 'yaml'
+require 'optparse'
 
 class Backuper
 end
 
 class ConfigParser
-  @@config_name='~/.rb-backuper/config.yml1'
+  @@config_name='~/.rb-backuper/config.yml'
   @@config
+  def config_name=(name)
+    @@config_name = name
+  end
   def reader
     @@config = File.open(File.expand_path(@@config_name)) do |io| 
       YAML::load(io) 
     end
     puts @@config
   rescue => detail
-    puts "Something bad happened when tryeing to read config:\n" + detail.message
+    puts "Something bad happened when trying to read config:\n" + detail.message
   end
 end
 
-ConfigParser.new.reader
+options={}
+
+optparse = OptionParser.new do|opts|
+  # Set a banner, displayed at the top
+  # of the help screen.
+  opts.banner = "Usage: rb-backuper.rb -c config_file"
+ 
+  options[:config_file] = nil
+  opts.on( '-c', '--config FILE', 'Read config from FILE, default ~/.rb-backuper/config.yml' ) do|file|
+    options[:config_file] = file
+  end
+ 
+   # This displays the help screen, all programs are
+   # assumed to have this option.
+   opts.on( '-h', '--help', 'Display this screen' ) do
+     puts opts
+     exit
+  end
+end.parse!
+
+#optparse.parse
+parser=ConfigParser.new
+parser.config_name = options[:config_file] if options[:config_file]
+parser.reader
